df2e66f376c3d2108170e1f7722ec6bded751049
lnxtha <linusxrstha@gmail.com>
2019-01-23 14:42:24 +0545

    a

--- src/package_recommend/settings.py
+++ src/package_recommend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bc0@+pl(_yq-b^9qb0mbvn0ys@38m8dp16!khtzdos5mlq+&yy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
