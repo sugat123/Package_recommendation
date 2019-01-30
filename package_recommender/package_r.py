1ff59bad570973ea0b476e47487561d5cda40a7c
Sugat Paneru <sugatp454@gmail.com>
2019-01-30 15:47:17 +0545

    history_recommend

--- package_recommender/package_r.py
+++ package_recommender/package_r.py
@@ -19,10 +19,15 @@ interactions = create_interaction_matrix(df = ratings,
                                          user_col = 'UserId',
                                          item_col = 'PackageId',
                                          rating_col = 'Rating')
-interactions.head()
+
+interactions_history = create_interaction_matrix(df = ratings,
+                                         user_col = 'UserId',
+                                         item_col = 'PackageId',
+                                         rating_col = 'timestamp')
 
 # Create User Dict
 user_dict = create_user_dict(interactions=interactions)
+user_dict_history = create_user_dict(interactions=interactions_history)
 # Create Item dict
 packages_dict = create_item_dict(df = packages,
                                id_col = 'ID',
@@ -34,11 +39,25 @@ mf_model = runMF(interactions = interactions,
                  epoch = 30,
                  n_jobs = 4)
 
+mf_model = runMF(interactions = interactions_history,
+                 n_components = 30,
+                 loss = 'warp',
+                 epoch = 30,
+                 n_jobs = 4)
 ## Calling 10 movie recommendation for user id 2
 rec_list = sample_recommendation_user(model = mf_model, 
                                       interactions = interactions, 
-                                      user_id = 6, 
+                                      user_id = 2, 
                                       user_dict = user_dict,
+                                      item_dict = packages_dict, 
+                                      threshold = 4,
+                                      nrec_items = 5,
+                                      show = True)
+
+rec_list_history = sample_recommendation_user(model = mf_model, 
+                                      interactions = interactions_history, 
+                                      user_id = 2, 
+                                      user_dict = user_dict_history,
                                       item_dict = packages_dict, 
                                       threshold = 4,
                                       nrec_items = 5,
